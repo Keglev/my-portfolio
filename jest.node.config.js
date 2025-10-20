7b14a1ccd51d6345cddde2a3552b19a478f2fd96
Keglev <ckbuzin1@gmail.com>
2025-10-20 12:32:50 +0200

    ci: update jest.node.config and project media meta

--- jest.node.config.js
+++ jest.node.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  setupFiles: ['<rootDir>/config/jest/setupTestsNode.js'],
+  // Use setupFilesAfterEnv so testing-library/jest-dom matchers are installed
+  // into the Jest environment before tests run.
+  setupFilesAfterEnv: ['<rootDir>/config/jest/setupTestsNode.js'],
   transformIgnorePatterns: ['node_modules/(?!(axios)/)'],
   moduleNameMapper: {
     '^.+\\.(css|less|scss|sass)$': '<rootDir>/config/jest/styleMock.js',
