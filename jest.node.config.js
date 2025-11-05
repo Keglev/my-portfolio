f396816e5825bc910c2e29a99ed714d45c4d45ae
Keglev <ckbuzin1@gmail.com>
2025-11-05 10:47:36 +0100

    Add Babel transform to Jest node config to fix CI test failures

--- jest.node.config.js
+++ jest.node.config.js
@@ -3,11 +3,13 @@ module.exports = {
   // Use setupFilesAfterEnv so testing-library/jest-dom matchers are installed
   // into the Jest environment before tests run.
   setupFilesAfterEnv: ['<rootDir>/config/jest/setupTestsNode.js'],
+  transform: {
+    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': ['babel-jest', { presets: ['react-app'] }]
+  },
   transformIgnorePatterns: ['node_modules/(?!(axios)/)'],
   moduleNameMapper: {
     '^.+\\.(css|less|scss|sass)$': '<rootDir>/config/jest/styleMock.js',
-    '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/config/jest/fileMock.js'
-    ,
+    '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/config/jest/fileMock.js',
     '^react-dom/test-utils$': '<rootDir>/config/jest/react-dom-test-utils.js'
   }
 };
